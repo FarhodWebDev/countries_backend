0c50e9ec23134f3764159afa678eac7b811ca824
FarhodWebDev <yusupov0902farod@gmail.com>
2023-07-13 12:32:36 +0500

    xex, i forget smth

--- src/config/db.js
+++ src/config/db.js
@@ -3,19 +3,19 @@
 import { Sequelize } from "sequelize";
 
 // --- LOCAL ---
-const db = new Sequelize("countries", "postgres", "2251", {
- port: 6651,
- user: "postgres",
- password: "2251",
- host: "localhost",
- database: "countries",
- dialect: "postgres",
-});
+// const db = new Sequelize("countries", "postgres", "2251", {
+//  port: 6651,
+//  user: "postgres",
+//  password: "2251",
+//  host: "localhost",
+//  database: "countries",
+//  dialect: "postgres",
+// });
 
 //  --- ONLINE ---
-// const db = new Sequelize(
-// "postgres://countries_udrl_user:g45gjdLAG4O596ysDms7JE20pIQ8kdij@dpg-cime03lgkuvgvhfalq0g-a.oregon-postgres.render.com/countries_udrl"
-// );
+const db = new Sequelize(
+"postgres://countries_udrl_user:g45gjdLAG4O596ysDms7JE20pIQ8kdij@dpg-cime03lgkuvgvhfalq0g-a.oregon-postgres.render.com/countries_udrl"
+);
 
 try {
  await db.authenticate();
